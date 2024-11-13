2162db7005b15f4450032afa0645028da7b1de32
astraore27 <185303692+astraore27@users.noreply.github.com>
2024-11-12 19:27:00 -0600

    Add files via upload

---
+++ dj.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE KaraokeDB;
+USE Karaoke;
+
+CREATE TABLE User (
+    user_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+);
+
+CREATE TABLE Artist(
+  name VARCHAR(255) PRIMARY KEY,
+
+);
+
+CREATE TABLE
