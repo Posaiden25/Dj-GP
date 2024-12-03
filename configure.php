611d2b8c4ab97ceb3ca93b9cb93e10f6b3dee0c7
Posaiden <polandrocks3@gmail.com>
2024-12-02 21:48:40 -0600

    Create configure.php

---
+++ configure.php
@@ -0,0 +1,25 @@
+<?php
+require 'connection.php';
+
+
+function executeFile(PDO $pdo, $filePath) {
+    
+        $sql = file_get_contents($filePath);
+
+         if ($sql === false) {
+             throw new Exception("No file: $filePath");
+        }       
+
+        $pdo->exec($sql);
+        echo "worked";
+}   
+
+try{
+
+    executeFile($pdo, 'dj.sql');
+    executeFile($pdo, 'insert.sql');
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    exit();
+}
+?>
