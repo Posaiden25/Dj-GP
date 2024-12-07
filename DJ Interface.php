074c33bcf8d6c45cf5e2b9df578bc806f1f53fb6
Colton! <143637463+ColtonDean2003@users.noreply.github.com>
2024-12-06 19:04:49 -0600

    Update DJ Interface.php

--- DJ Interface.php
+++ DJ Interface.php
@@ -1,6 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head><title>Karaoke Disc Jockey</title><head>
+<style>
+        body {
+            background-color:  gray;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            color: black;
+            font-size: 70px;
+            text-align: center;
+        }
+        form {
+            background-color: black;
+            padding: 5px;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+            width: 200px;
+            margin: 10px;
+        }
+        input[type="submit"] {
+            color: black;
+            border: none;
+            padding: 10px 15px;
+            border-radius: 8px;
+            cursor: pointer;
+            background-color:#ff6347;
+        }
+        input[type="submit"]:hover {
+            opacity: 0.5;
+        }
+        table {
+            border-collapse: none;
+            width: 100%;
+            background-color:  dark gray;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.6);
+        }
+        th, td {
+            border: 5px solid black;
+            padding: 5px;
+            color: black;
+        }
+        th {
+            background-color: #ff6347;
+        }
+        td {
+            text-align: center;
+        }
+        .error {
+            color: #ff6347;
+            margin-top: 10px;
+            text-align: center;
+        }
+        .success {
+            color: green;
+            margin-top: 10px;
+            text-align: center;
+        }
+</style>
+
+
 <?php
 // Database connection setup
 $host = 'courses';
@@ -51,14 +111,14 @@ catch (PDOException $e)
                         $result->execute(array('id' => $_POST["ID"]));
                         if ($result->rowCount() == 0) // if it's not there
                         {
-                                echo "Invalid option, did you refresh the page?";
+                            echo "<p class='error'>Invalid option, did you refresh the page?</p>";
                         }
                         else // deletes it :)
                         {
                                 $result = $pdo->prepare("DELETE FROM " . $table . " WHERE " . $id . " = :id");
                                 $result->execute(array('id' => $_POST["ID"]));
                                 // bye bye :)
-                                echo "Song is now playing. Removed from queue";
+                                echo "<p class='success'>Song is now playing. Removed from queue</p>";
                         }
                 }
                 echo "</p>";
